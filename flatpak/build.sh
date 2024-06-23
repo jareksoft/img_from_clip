b66bf138013744c8c4b2210928e2f85c947a8ae2
Jarosław Pelczar <jarek@jpelczar.com>
2024-06-23 06:16:46 +0200

    Update build script also for AArch64
    Signed-off-by: Jarosław Pelczar <jarek@jpelczar.com>

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -4,4 +4,5 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 flatpak-builder --repo=repo_dir --arch=x86_64 --install-deps-from=flathub --ccache --force-clean build_dir_x86_64 com.jpelczar.img_from_clip.json
 flatpak-builder --repo=repo_dir_aarch64 --arch=aarch64 --install-deps-from=flathub --ccache --force-clean build_dir_aarch64 com.jpelczar.img_from_clip.json
 
-# flatpak build-bundle repo_dir ImgFromClip-x86_64.flatpak com.jpelczar.img_from_clip main
+flatpak build-bundle repo_dir ImgFromClip-x86_64.flatpak com.jpelczar.img_from_clip
+flatpak build-bundle repo_dir_aarch64 ImgFromClip-aarch64.flatpak com.jpelczar.img_from_clip
