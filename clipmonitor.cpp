9530134bead00535a6882e3c98d32f6f2dab60cb
Jarosław Pelczar <jarek@jpelczar.com>
2024-06-29 15:13:32 +0200

    Update files to dual license
    Signed-off-by: Jarosław Pelczar <jarek@jpelczar.com>

--- clipmonitor.cpp
+++ clipmonitor.cpp
@@ -1,4 +1,6 @@
+// This file is licensed under either of licenses:
 // SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: BSD-2-Clause
 
 #include "clipmonitor.h"
 #include "rsvg_helper.h"
